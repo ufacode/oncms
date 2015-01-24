0ee0922dd6bf904bfcc431c32ae166753799d970
MpaK <mrak69@gmail.com>
2015-01-24 18:25:54 +0700

    Readme changed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,6 +35,11 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
+  %w(stylesheets javascripts fonts images).each do |sub|
+    config.assets.paths << ::Bootstrap::Rails::Engine.root.join('assets', sub)
+  end
+  config.assets.precompile << %r(bootstrap/glyphicons-halflings-regular\.(?:eot|svg|ttf|woff)$)
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
